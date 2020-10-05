781bd1d7e57461a9c231f818bdd4e675a8502106
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-05 16:45:10 +0900

    login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (e) => {
+    // 아래와 같음: const { name, value } = e.target;
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <span>Auth</span>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
     </>
   );
 };
