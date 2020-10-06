dfaa45d883e2fbb183eefcf5b4c44d38bf99a4b2
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-06 16:49:46 +0900

    delete update

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,14 @@ import { authService } from "fbase";
 
 const App = () => {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj(user);
       } else {
-        setIsLoggedIn(false);
+        setUserObj(null);
       }
       setInit(true);
     });
@@ -22,7 +20,7 @@ const App = () => {
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
       ) : (
         "Initializing..."
       )}
--- src/components/Aweet.js
+++ src/components/Aweet.js
@@ -1,18 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Aweet.css";
+import { dbService } from "fbase";
 
 const Aweet = ({ aweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newAweet, setNewAweet] = useState(aweetObj.text);
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this aweet?");
+    if (ok) {
+      await dbService.doc(`aweets/${aweetObj.id}`).delete();
+    }
+  };
+  const toggleEditing = () => {
+    setEditing((prev) => !prev);
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.doc(`aweets/${aweetObj.id}`).update({
+      text: newAweet,
+    });
+    setEditing(false);
+  };
+  const onChange = (e) => {
+    const { value } = e.target;
+    setNewAweet(value);
+  };
   return (
     <>
-      <div>
-        <h4>{aweetObj.text}</h4>
-        <h5>{String(new Date(aweetObj.createdAt)).substring(0, 24)}</h5>
-        {isOwner && (
-          <>
-            <button>Delete Aweet</button>
-            <button>Edit Aweet</button>
-          </>
-        )}
-      </div>
+      {editing ? (
+        <>
+          <div className="aweet">
+            <form onSubmit={onSubmit}>
+              <input
+                type="text"
+                placeholder="Edit your aweet"
+                value={newAweet}
+                onChange={onChange}
+                required
+              />
+              <input type="submit" value="Update Aweet" />
+            </form>
+            <button onClick={toggleEditing}>cancel</button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="aweet">
+            <h4>{aweetObj.text}</h4>
+            <h5>{String(new Date(aweetObj.createdAt)).substring(0, 24)}</h5>
+            {isOwner && (
+              <div className="aweet__button">
+                <button onClick={onDeleteClick}>Delete Aweet</button>
+                <button onClick={toggleEditing}>Edit Aweet</button>
+              </div>
+            )}
+          </div>
+        </>
+      )}
     </>
   );
 };
