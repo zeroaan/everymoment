68ace1c4161c05c23ec110ffa475927e04324557
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-09 17:20:46 +0900

    Updates

---
+++ precache-manifest.e8ce1c729278b4fb02be9d96620af8f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252de85f14edc2cd88251affb2591ee3",
+    "url": "/awitter/index.html"
+  },
+  {
+    "revision": "fd24d5a1e04f82f69899",
+    "url": "/awitter/static/css/main.4f1444d6.chunk.css"
+  },
+  {
+    "revision": "4be8dcf081e848528232",
+    "url": "/awitter/static/js/2.96c8f64f.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/awitter/static/js/2.96c8f64f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd24d5a1e04f82f69899",
+    "url": "/awitter/static/js/main.33462cea.chunk.js"
+  },
+  {
+    "revision": "285150e5afc7ff9b35e6",
+    "url": "/awitter/static/js/runtime-main.001eb6ae.js"
+  }
+]);
